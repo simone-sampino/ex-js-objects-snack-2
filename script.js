13f78dcef2d80fc37c29741c5e695d41241c7e00
simone-sampino <s.samp98@gmail.com>
2025-10-28 06:00:20 +0100

    Add initial HTML and JavaScript files for Snack Oggetti project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Code Question 1
+
+/* const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // ?
+console.log(secondBurger.name ); // ? */
